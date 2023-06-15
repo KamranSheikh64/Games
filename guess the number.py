cf4dad2e6b0590384c1f3dc39f44200833411c8b
KamranSheikh64 <136167698+KamranSheikh64@users.noreply.github.com>
2023-06-15 12:56:51 +0530

    Add files via upload

---
+++ guess the number.py
@@ -0,0 +1,21 @@
+x=int(input('guess the number:'))
+if x==5:
+	print('`````````````cogratulation u r right`````````````````')
+elif x==4:
+	print('You are very close')
+elif x==6:
+	print('You are very close')
+elif x==3:
+	print('You are not much close')
+elif x==7:
+	print('you arenot much close')
+elif x==8:
+	print('You are very far')
+elif x==2:
+	print('You are very far')
+elif x==9:
+	print('You are very very very far')
+elif x==1:
+	print('You are very very very far')
+else:
+	print('it is far away from ur thinking')
